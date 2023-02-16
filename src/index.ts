6ba74815277534079dced43902ead22c4d72055e
Kumar Kartikay <krkartikay@gmail.com>
2023-02-16 17:20:25 +0530

    waves

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,43 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, FederatedPointerEvent, Graphics, Sprite, Texture } from 'pixi.js'
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
-	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+	backgroundColor: 0x111111,
+	width: window.innerWidth,
+	height: window.innerHeight,
 });
 
-const clampy: Sprite = Sprite.from("clampy.png");
+const background_sprite : Sprite = new Sprite(Texture.EMPTY);
 
-clampy.anchor.set(0.5);
+const waves : Graphics[] = [];
 
-clampy.x = app.screen.width / 2;
-clampy.y = app.screen.height / 2;
+function init() {
+	background_sprite.width = window.innerWidth;
+	background_sprite.height = window.innerHeight;
+	app.stage.addChild(background_sprite);
+	background_sprite.interactive = true;
+	background_sprite.on('click', accept_click);
+}
 
-app.stage.addChild(clampy);
+function accept_click(click_evt: FederatedPointerEvent) {
+	let x = click_evt.x;
+	let y = click_evt.y;
+	let new_wave = new Graphics;
+	new_wave.lineStyle(1, 0xff0000);
+	new_wave.drawCircle(x, y, 10);
+	new_wave.x = 0;
+	new_wave.y = 0;
+	waves.push(new_wave);
+	app.stage.addChild(new_wave);
+}
+
+function update() {
+	for (const wave of waves) {
+		wave.scale.set(wave.scale.x + 0.01);
+	}
+}
+
+init();
+setInterval(update, 10);
